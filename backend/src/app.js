70500c5026750b8cf44543164dd3531abcff64c6
juan garcia <juangarcia@unimayor.edu.co>
2026-09-15 21:44:29 -0500

    feat(sprint 1) agregar surta de test y endopoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const rutaAutenticacion = require(
     "./modules/auth/rutas/rutaAutenticacion"
 );
+const rutaTest = require("../routes/test.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", rutaAutenticacion);
-
+app.use("/api", rutaTest);
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
 module.exports = app;
