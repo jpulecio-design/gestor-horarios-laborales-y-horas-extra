b3cff52f965e5c07885cb08eec96382ed27bd4ce
Ivvanw <ichimunja@unimayor.edu.co>
2026-09-16 20:37:09 -0500

    Revert "Ivan beltran dev"

--- backend/src/modules/horas-extra/repositorios/estado.js
+++
@@ -1,6 +0,0 @@
-let estado = {
-    horasExtras: [],  // array que toma los objetos hora extra
-    contadorId: 1     // aqui se le asigna a c/hora extra un id distinto
-};
-
-module.exports = estado;
--- backend/src/modules/nomina/repositorios/estado.js
+++
@@ -1,6 +0,0 @@
-let estado = {
-    nominas: [],      // array que toma los objetos nomina
-    contadorId: 1     // aqui se le asigna a c/nomina un id distinto
-};
-
-module.exports = estado;
