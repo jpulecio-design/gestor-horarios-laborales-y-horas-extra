0d2b10fed269be1a226d61103ccfdfdf141dcd62
Ivan Beltran <ichimunja@unimayor.edu.co>
2026-09-16 20:00:36 -0500

    Prueba integracion

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 
 const rutaAutenticacion = require("./modules/auth/rutaAutenticacion");
-<<<<<<< HEAD
 const rutaTest = require("./modules/test/rutas/rutaTest");
 const rutaEmpleados = require("./modules/empleados/rutas/rutaEmpleados");
 const rutaHorasExtra = require("./modules/horas-extra/rutas/rutaHorasExtra");
 const rutaNomina = require("./modules/nomina/rutas/rutaNomina");
 
-=======
-
-const rutaTest = require("../routes/test.routes");
->>>>>>> b0744e191c009c297844f3c80504fb631f114bdc
 const app = express();
 
 app.use(express.json());
